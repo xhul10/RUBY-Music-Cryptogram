e3d50cb77d2d80e40d783bf4df86a549933656b7
Receive03 <xhulio0031@gmail.com>
2016-12-19 22:11:49 -0800

    deciphers notes into alphabet div color arrays

--- lib/audiomancy.rb
+++ lib/audiomancy.rb
@@ -2,32 +2,32 @@
 class String
   define_method(:music_encryption) do
 
-    musicCipher = {"a" => "e",
-                   "b" => "e",
-                   "c" => "d#",
-                   "d" => "f#",
-                   "e" => "c",
-                   "f" => "c",
-                   "g" => "g#",
-                   "h" => "c",
-                   "i" => "f",
-                   "j" => "f",
-                   "k" => "a#",
-                   "l" => "f#",
-                   "m" => "g#",
-                   "n" => "g",
-                   "o" => "a",
-                   "p" => "c",
-                   "q" => "b",
-                   "r" => "d",
-                   "s" => "g#",
-                   "t" => "a",
-                   "u" => "c#",
-                   "v" => "c#",
-                   "w" => "c#",
-                   "x" => "a#",
-                   "y" => "f",
-                   "z" => "d",
+    musicCipher = {"a" => "<div class='col-md-1 e'>e</div>",
+                   "b" => "<div class='col-md-1 e'>e</div>",
+                   "c" => "<div class='col-md-1 dSharp'>d#</div>",
+                   "d" => "<div class='col-md-1 fSharp'>f#</div>",
+                   "e" => "<div class='col-md-1 c'>c</div>",
+                   "f" => "<div class='col-md-1 c'>c</div>",
+                   "g" => "<div class='col-md-1 gSharp'>g#</div>",
+                   "h" => "<div class='col-md-1 c'>c</div>",
+                   "i" => "<div class='col-md-1 f'>f</div>",
+                   "j" => "<div class='col-md-1 f'>f</div>",
+                   "k" => "<div class='col-md-1 aSharp'>a#</div>",
+                   "l" => "<div class='col-md-1 fSharp'>f#</div>",
+                   "m" => "<div class='col-md-1 gSharp'>g#</div>",
+                   "n" => "<div class='col-md-1 g'>g</div>",
+                   "o" => "<div class='col-md-1 a'>a</div>",
+                   "p" => "<div class='col-md-1 c'>c</div>",
+                   "q" => "<div class='col-md-1 b'>b</div>",
+                   "r" => "<div class='col-md-1 d'>d</div>",
+                   "s" => "<div class='col-md-1 gSharp'>g#</div>",
+                   "t" => "<div class='col-md-1 a'>a</div>",
+                   "u" => "<div class='col-md-1 cSharp'>c#</div>",
+                   "v" => "<div class='col-md-1 cSharp'>c#</div>",
+                   "w" => "<div class='col-md-1 cSharp'>c#</div>",
+                   "x" => "<div class='col-md-1 aSharp'>a#</div>",
+                   "y" => "<div class='col-md-1 f'>f</div>",
+                   "z" => "<div class='col-md-1 d'>d</div>",
                    " " => ""}
 
     outputArray = []
@@ -44,18 +44,18 @@ class String
 
   define_method(:music_decryption) do
 
-    musicCipher = {"e" => "a / b",
-                   "d#" => "c",
-                   "f#" => "d / l",
-                   "c" => "e / f / h / p",
-                   "g#" => "g / m / s",
-                   "f" => "i / j / y",
-                   "a#" => "k / x",
-                   "g" => "n",
-                   "a" => "o / t",
-                   "b" => "q",
-                   "d" => "r / z",
-                   "c#" => "u / v / w",
+    musicCipher = {"e" => "<div class='col-md-1 e'>a</div><div class='col-md-1 e'>b</div>",
+                   "d#" => "<div class='col-md-1 dSharp'>c</div>",
+                   "f#" => "<div class='col-md-1 fSharp'>d</div><div class='col-md-1 fSharp'>l</div>",
+                   "c" => "<div class='col-md-1 c'>e</div><div class='col-md-1 c'>f</div><div class='col-md-1 c'>h</div><div class='col-md-1 c'>p</div>",
+                   "g#" => "<div class='col-md-1 gSharp'>g</div><div class='col-md-1 gSharp'>m</div><div class='col-md-1 gSharp'>g</div>",
+                   "f" => "<div class='col-md-1 f'>i</div><div class='col-md-1 f'>j</div><div class='col-md-1 f'>y</div>",
+                   "a#" => "<div class='col-md-1 aSharp'>k</div><div class='col-md-1 aSharp'>x</div>",
+                   "g" => "<div class='col-md-1 g'>n</div>",
+                   "a" => "<div class='col-md-1 a'>o</div><div class='col-md-1 a'>t</div>",
+                   "b" => "<div class='col-md-1 b'>q</div>",
+                   "d" => "<div class='col-md-1 d'>r</div><div class='col-md-1 d'>z</div>",
+                   "c#" => "<div class='col-md-1 cSharp'>u</div><div class='col-md-1 cSharp'>v</div><div class='col-md-1 cSharp'>w</div>",
                    " " => ""}
 
     outputArray = []
@@ -77,6 +77,6 @@ counter = 0
         counter = counter + 1
       end
     end
-    outputArray.join(" |  +  | ")
+    outputArray.join("")
   end
 end
