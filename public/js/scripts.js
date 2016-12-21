14439bb07d4173c3919aebf7c1e1d4a6ea2e8e6b
Receive03 <xhulio0031@gmail.com>
2016-12-21 08:52:36 -0800

    add music notes to audio folder and trigger when div is clicked

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,19 +1,63 @@
 $(document).ready(function() {
-  $(".b").click(function(event) {
-  var audioElement = document.createElement('audio');
 
-  $("audioElement").setAttribute('src', '/audio/bnote.wav');
-  $("audioElement").setAttribute('autoplay', 'autoplay');
-  $("audioElement").play();
-  event.preventDefault();
-  }
-  
-  $('.b').keypress(function (e) {
-   var key = e.which;
-   if(key == 13)  // the enter key code
-    {
-      $('.b').click()  ;
-      return false;
-    }
+  $(".c").click(function() {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', '/audio/c.wav');
+    audioElement.play();
+  });
+  $(".cSharp").click(function() {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', '/audio/cSharp.wav');
+    audioElement.play();
+  });
+  $(".d").click(function() {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', '/audio/d.wav');
+    audioElement.play();
+  });
+  $(".dSharp").click(function() {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', '/audio/dSharp.wav');
+    audioElement.play();
+  });
+  $(".e").click(function() {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', '/audio/e.wav');
+    audioElement.play();
+  });
+  $(".f").click(function() {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', '/audio/f.wav');
+    audioElement.play();
+  });
+  $(".fSharp").click(function() {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', '/audio/fSharp.wav');
+    audioElement.play();
+  });
+  $(".g").click(function() {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', '/audio/g.wav');
+    audioElement.play();
+  });
+  $(".gSharp").click(function() {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', '/audio/gSharp.wav');
+    audioElement.play();
+  });
+  $(".a").click(function() {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', '/audio/a.wav');
+    audioElement.play();
+  });
+  $(".aSharp").click(function() {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', '/audio/aSharp.wav');
+    audioElement.play();
+  });
+  $(".b").click(function() {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', '/audio/b.wav');
+    audioElement.play();
   });
 });
