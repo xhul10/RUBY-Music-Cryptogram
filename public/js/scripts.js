0976a08d892c5fbeb31e7cfb0bce2c88bd76430a
Receive03 <xhulio0031@gmail.com>
2016-12-20 22:32:15 -0800

    attempt to build out jquery, no success yet

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,10 +1,19 @@
 $(document).ready(function() {
-    $(".b").click(function(event) {
-    var audioElement = document.createElement('audio');
+  $(".b").click(function(event) {
+  var audioElement = document.createElement('audio');
 
-    $("audioElement").setAttribute('src', '/audio/bnote.wav');
-    $("audioElement").setAttribute('autoplay', 'autoplay');
-    $("audioElement").play();
-    event.preventDefault();
+  $("audioElement").setAttribute('src', '/audio/bnote.wav');
+  $("audioElement").setAttribute('autoplay', 'autoplay');
+  $("audioElement").play();
+  event.preventDefault();
   }
+  
+  $('.b').keypress(function (e) {
+   var key = e.which;
+   if(key == 13)  // the enter key code
+    {
+      $('.b').click()  ;
+      return false;
+    }
+  });
 });
